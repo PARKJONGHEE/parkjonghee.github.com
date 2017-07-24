4fe310e98527b24f17102364062d82af83da2af1
DESKTOP-KISN4N0\3dit <prog.pjh@gmail.com>
2017-07-24 15:53:50 +0900

    home design commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// $(function() {
+//     $('a.page-scroll').bind('click', function(event) {
+//         var $anchor = $(this);
+//         $('html, body').stop().animate({
+//             scrollTop: $($anchor.attr('href')).offset().top
+//         }, 1500, 'easeInOutExpo');
+//         event.preventDefault();
+//     });
+// });
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
